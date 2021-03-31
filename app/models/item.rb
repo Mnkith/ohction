5d84aa4c0a526a883049e956e8576be559a84692
Mnkith <munkithabid@gmail.com>
2021-03-31 00:47:25 -0500

    create item helpers

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
   belongs_to :buyer, class_name:  'User', foreign_key:  'buyer_id', optional: true # buyer_id won't be set
   belongs_to :seller, class_name:  'User', foreign_key:  'seller_id'               # until someone buyes the item
                                                                                    # hence optional: true
-
+  has_many :item_details
+  has_many :images, through: :item_details
+  # has_many :bulletings, through: :item_datails
 end
  
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Item.destroy_all
+Image.destroy_all
+User.destroy_all
+# ItemDetail.destroy_all
+
+s1 = User.create name: 'seller_1', email: 's1@ohction,com', password_digest: '123'
+s2 = User.create name: 'seller_2', email: 's2@ohction,com', password_digest: '123'
+
+im1= Image.create image_path: 'item1_1.jpg'
+im2= Image.create image_path: 'item1_2.jpg'
+im3= Image.create image_path: 'item1_3.jpg'
+im4= Image.create image_path: 'item1_4.jpg'
+
+i1 = Item.create title: 'item_1_titl', seller_id: s1.id
+i2 = Item.create title: 'item_2_titl', seller_id: s1.id
+i3 = Item.create title: 'item_3_titl', seller_id: s1.id
+i4 = Item.create title: 'item_4_titl', seller_id: s1.id
+i5 = Item.create title: 'item_5_titl', seller_id: s1.id
+i6 = Item.create title: 'item_6_titl', seller_id: s2.id
+ItemDetail.create item_id: i1.id, image_id: im1.id
+ItemDetail.create item_id: i1.id, image_id: im2.id
+ItemDetail.create item_id: i1.id, image_id: im3.id
+ItemDetail.create item_id: i1.id, image_id: im4.id
+# i1.images << im1 << im2 << im3 << im4
+
