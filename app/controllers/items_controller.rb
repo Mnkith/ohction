a792c79f7e1586eaaf66e333aa0fd0fb3d46ffac
Mnkith <munkithabid@gmail.com>
2021-04-02 11:52:38 -0500

    finish up image upload features

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  layout false
+  # layout false
   def index 
     @items = Item.all
   end
@@ -20,7 +20,10 @@ class ItemsController < ApplicationController
 
   def create 
     @item = Item.new item_params
-    if @item.save
+    if @item.valid?
+      images = images_params["images_attributes"]
+      upload_images @item, images
+      @item.save
       redirect_to @item, notice: 'Your item was added successfully.'
     else
       binding.pry
@@ -34,7 +37,21 @@ class ItemsController < ApplicationController
   private
    
   def item_params
-    params.require(:item).permit(:title, :seller_id, :start_time, :end_time, images_attributes:{})
+    params.require(:item).permit(:title, :seller_id, :start_time, :end_time)
+  end
+
+  def images_params
+    params.require(:item).permit(images_attributes: {})
+  end
+
+  def upload_images(item, images)
+    images.each do |_, image|
+      uploaded_file = image[:path]
+      File.open(Rails.root.join('app', 'assets', 'images', uploaded_file.original_filename), 'wb') do |file|
+        file.write(uploaded_file.read)
+      end
+      item.images << Image.create(path: uploaded_file.original_filename)
+    end
   end
 
   
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,7 @@
 class Item < ApplicationRecord
-
-  belongs_to :buyer, class_name:  'User', foreign_key:  'buyer_id', optional: true # buyer_id won't be set
-  belongs_to :seller, class_name:  'User', foreign_key:  'seller_id'               # until someone buyes the item
-                                                                                   # hence optional: true
+  belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id', optional: true # buyer_id won't be set
+  belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'               # until someone buyes the item
+  # hence optional: true
   has_many :item_details
   has_many :images, through: :item_details
   has_many :bulletings, through: :item_details
@@ -12,21 +11,28 @@ class Item < ApplicationRecord
   validate :start_time_must_be_in_futur
   validate :end_time_later_than_start_time
 
-
   def start_time_must_be_in_futur
-    if start_time
-      errors.add(:start_time, "must be in the future!") if start_time&.strftime('%Y-%m-%d %H:%M:%S') < Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    if start_time && (start_time.strftime('%Y-%m-%d %H:%M:%S') < Time.now.strftime('%Y-%m-%d %H:%M:%S'))
+      errors.add(:start_time,
+                 'must be in the future!')
     end
   end
 
   def end_time_later_than_start_time
-    if start_time && end_time 
-      errors.add(:end_time, "must be ahead of start time!") if start_time > end_time
-    end
+    errors.add(:end_time, 'must be ahead of start time!') if start_time && end_time && (start_time > end_time)
   end
   # validates :title, presence: true
   # validates :starting_price, presence: true, numericality: { only_integer: true }
-  # validates :duration, presence: true 
-  # validates :shipping, presence: true  
+  # validates :duration, presence: true
+  # validates :shipping, presence: true
+
+  # def upload_images(item, paths)
+  #   paths.each do |_, _v|
+  #     uploaded_file = [:path]
+  #     File.open(Rails.root.join('app', 'assets', 'images', uploaded_file.original_filename), 'wb') do |file|
+  #       file.write(uploaded_file.read)
+  #     end
+  #     item.images << Image.create(path: uploaded_file.original_filename)
+  #   end
+  # end
 end
- 
