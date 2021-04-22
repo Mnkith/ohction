150d2ac31a8dce401a02b87fcc3361b01d0cbc13
Mnkith <munkithabid@gmail.com>
2021-04-22 14:34:07 -0500

    update bid_params

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,15 +1,15 @@
 class BidsController < ApplicationController
   def create
-    @bid = Bid.new params.permit(:item_id).merge params.require(:bid).permit(:amount)
-    @bid.user = current_user
+    @bid = bid_params
     if @bid.valid?
-      # item = @bid.item
-      # item.current_price += @bid.amount
-      # item.update_status
-      # item.set_sold if item.sold?
-      # item.save
       @bid.save
     end
     redirect_to item_path(@bid.item), alert: @bid.errors.full_messages[0]
+  end
+
+  private
+
+  def bid_params
+    Bid.new params.permit(:item_id, :amount).merge({user: current_user})
   end
 end
