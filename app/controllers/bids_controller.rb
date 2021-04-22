559f1adfa70d63ff50e1f03d13111401ffbc29e1
Mnkith <munkithabid@gmail.com>
2021-04-22 14:13:54 -0500

    refactor bid code into before_save call back method

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -3,11 +3,11 @@ class BidsController < ApplicationController
     @bid = Bid.new params.permit(:item_id).merge params.require(:bid).permit(:amount)
     @bid.user = current_user
     if @bid.valid?
-      item = @bid.item
-      item.current_price += @bid.amount
-      item.update_status
+      # item = @bid.item
+      # item.current_price += @bid.amount
+      # item.update_status
       # item.set_sold if item.sold?
-      item.save
+      # item.save
       @bid.save
     end
     redirect_to item_path(@bid.item), alert: @bid.errors.full_messages[0]
--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,5 +2,15 @@ class Bid < ApplicationRecord
   belongs_to :user
   belongs_to :item
 
-  validates :amount, numericality: { only_integer: true, greater_than: 0 }
+  before_save :update_item
+
+
+  validates :amount, numericality: { greater_than: 0 }
+
+  def update_item
+    item = self.item
+    item.current_price += amount
+    item.update_status
+    item.save
+  end
 end
